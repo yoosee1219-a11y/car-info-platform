1d9cdd230b2b6adca05cf25968bccb71da4795ff
yoosee1219-a11y <yoosee1219@gmail.com>
2025-09-30 19:28:17 +0900

    ?ル낫?? ?댁쟾?먮낫??異붽? - 4x4 洹좏삎 ?덉씠?꾩썐

--- src/components/QuickQuote.js
+++ src/components/QuickQuote.js
@@ -8,6 +8,8 @@ function QuickQuote() {
     { icon: "🏢", title: "암보험", desc: "든든한 보장" },
     { icon: "🏠", title: "주택화재", desc: "내집 지키기" },
     { icon: "👶", title: "어린이보험", desc: "우리 아이 보장" },
+    { icon: "🐶", title: "펫보험", desc: "반려동물 보장" },
+    { icon: "🚘", title: "운전자보험", desc: "운전자 보호" },
   ];
 
   return (
